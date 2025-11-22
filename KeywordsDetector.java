def4ac30c73ffc4be9e2d24bcd20ddb14964ffbf
Benjamin Zeligson <benjamin.zeligson@post.runi.ac.il>
2025-11-23 01:14:17 +0200

    Completed assignment

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -26,16 +26,58 @@ public class KeywordsDetector {
     public static void detectAndPrint(String[] sentences, String[] keywords) {
         for (int i = 0; i < sentences.length; i++) {
             String sentenceLowerCased = sentences[i];
-            sentenceLowerCased = MyString.lowerCase(sentenceLowerCased);
+            sentenceLowerCased = lowerCase(sentenceLowerCased);
             for (int j = 0; j < keywords.length; j++) {
                 String keywordLowerCased = keywords[j];
-                keywordLowerCased = MyString.lowerCase(keywordLowerCased);
-                if (MyString.contains(sentenceLowerCased,keywordLowerCased)){
+                keywordLowerCased = lowerCase(keywordLowerCased);
+                if (contains(sentenceLowerCased,keywordLowerCased)){
                     System.out.println(sentences[i]);
                     break;
                 }
             }
             
         }
+    }
+
+    public static boolean contains(String str1, String str2) {
+        if (str1.length() == 0 || str2.length() == 0){
+            return true;
+        }
+        char firstLetter = str2.charAt(0);
+        // index that will be checked in larger string
+        int fromIndex = 0;
+        do { 
+
+            int index = str1.indexOf(firstLetter, fromIndex);
+            if (index == -1)
+            {
+                //if the first letter of the contained string 
+                //doesn't appear it will not be in the bigger string
+                return false;
+            }
+            if (index + str2.length() <= str1.length()){
+                //checks if the first letter of the contained string is 
+                //actually the first letter of it inside the bigger string
+                if (str2.equals(str1.substring(index, index + str2.length()))){
+                    return true;
+                }
+            }
+            //checks from its next appearance in the bigger string 
+            fromIndex++;
+        }
+        while (fromIndex != 0);
+        return false;
+    }
+
+     public static String lowerCase(String str) {
+        String res = "";
+        for (int i = 0; i < str.length(); i++) { 
+            char ch = str.charAt(i);
+            if (ch >= 65 && ch <= 90){
+                ch = (char) ((int) ch + 32);
+            }
+            res = res + ch;
+        }
+        return res;
     }
 }
--- Primes.java
+++ Primes.java
@@ -2,26 +2,26 @@ public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
         System.out.println("Prime numbers up to " + n + ":");
-        boolean[] NumbersToN = new boolean[n];
+        boolean[] NumbersToN = new boolean[n+1];
         // sets all values from 2 to ne prime until proven else
-        for (int i = 2; i < n; i++) {
+        for (int i = 2; i < NumbersToN.length; i++) {
             NumbersToN[i] = true;
         }
         int primeCounter = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < NumbersToN.length; i++) {
             if (NumbersToN[i] == true) {
                 System.out.println(i);
                 primeCounter++;
                 // this while loop sets all multiples of the
                 // prime number (up to n) to be not prime
                 int j = 2;
-                while (i * j < n) { 
+                while (i * j < NumbersToN.length) { 
                     NumbersToN[i * j] = false;
                     j++;
                 }
             }
         }
-        double precentage = ((double) primeCounter / n) * 100;
+        double precentage = ((double) primeCounter / (NumbersToN.length - 1)) * 100;
         System.out.println("There are " + primeCounter + 
         " primes between 2 and " + n +" (" + (int) precentage +"% are primes)");
     }
